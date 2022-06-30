21898759c1faf1282ab7874e857f5829f245b241
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-06-30 16:28:10 +0800

    Created App, File & Folder structured, written base Part

---
+++ src/components/index.jsx
@@ -0,0 +1,13 @@
+import Navbar from './navbar/Navbar';
+import CTA from './cta/CTA';
+import Brand from './brand/Brand';
+import Feature from './feature/Feature';
+import Article from './article/Article'
+
+export {
+    Article,
+    Brand,
+    CTA,
+    Feature,
+    Navbar
+}
---
+++ src/containers/index.jsx
@@ -0,0 +1,10 @@
+import Blog from './blog/Blog'
+import Features from './features/Features'
+import Footer from './footer/Footer'
+import Header from './header/Header'
+import Possibility from './possibility/Possibilty'
+import WhatGPT3 from './whatGPT3/WhatGPT3'
+
+export {
+    Blog, Features, Footer, Possibility, WhatGPT3,Header
+}
