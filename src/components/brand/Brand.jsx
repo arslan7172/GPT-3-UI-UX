dc87c68aae97d6f31522569ee1283f7fd971c729
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-07-03 18:10:29 +0800

    Done Brands Part,but commited later

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
 import './brand.css';
+import images from '../../assets/index'
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={images.google} alt='Google__img'></img>
+      </div>
+      <div>
+        <img src={images.slack} alt='slack__img'></img>
+      </div>
+      <div>
+        <img src={images.atlassian} alt='atlassian__img'></img>
+      </div>
+      <div>
+        <img src={images.dropbox} alt='dropbox__img'></img>
+      </div>
+      <div>
+        <img src={images.shopify} alt='shopify__img'></img>
+      </div>
+    </div>
   )
 }
 
