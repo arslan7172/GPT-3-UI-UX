b136ef233819f59ae564768b5d2a758ef4a2fedb
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-07-03 19:28:59 +0800

    Done What is GPT-3 Part, some part of this part was done in the previous commit

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './feature.css';
-const Feature = ({title, text}) => {
+const Feature = ({ title, text }) => {
   return (
-    <div className='gpt3__features-container__feature'>
-      <div className='gpt3__features-container__feature-title'>
-        <div></div>
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
         <h1>{title}</h1>
       </div>
-      <div className='gpt3__features-container__feature-text'>
+      <div className="gpt3__features-container_feature-text">
         <p>{text}</p>
       </div>
     </div>
