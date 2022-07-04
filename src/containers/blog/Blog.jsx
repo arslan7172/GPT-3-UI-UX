67c6e03eaa8a8768c1d92b6848b8de9d0c83a523
Arslan Hasanov <hasanovarslan01.02.08@gmail.com>
2022-07-04 17:48:17 +0800

    Done Blog Part

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './blog.css'
+import Article from '../../components/article/Article'
+import images from '../../assets/index'
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={images.blog01} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+          <Article imgUrl={images.blog02} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={images.blog03} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={images.blog04} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+          <Article imgUrl={images.blog05} date="Sep 26, 2021" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
+        </div>
+      </div>
+    </div>
   )
 }
 
